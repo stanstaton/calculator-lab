9d1824c24b513a937780a66a20bf1d00ee06bc7d
h64 <hhongnr@gmail.com>
2019-07-29 22:29:49 -0700

    starter code

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*--- constants ---*/
+
+/*--- app's state ---*/
+var displayText;
+var inputValue;
+var currentOperation;
+
+/*--- cached element references ---*/
+const calculatorEl = document.querySelector('#calculator');
+const displayEl = document.querySelector('#display');
+
+/*--- event listeners ---*/
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Hello World!");
+})
+calculatorEl.addEventListener('click', handleClick);
+
+/*--- functions ---*/
+function handleClick(event) {
+    
+}
+function initialize() { 
+
+}
+function add(num1, num2) { 
+
+}
